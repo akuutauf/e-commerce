c3402ffadf35cfbaeb9f8ce2bb48ccfafec481f3
akuutauf <taufikhidayat09121@gmail.com>
2022-05-25 20:19:45 +0700

    Memperbaiki Responsive Navbar

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -16,17 +16,22 @@
 <body>
     {{-- Navabar Start --}}
     <section id="navbar">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-navbar fixed-top shadow  ">
+        <nav class="navbar navbar-expand-md navbar-dark bg-navbar fixed-top shadow  ">
             <div class="container d-flex justify-content-lg-around align-items-center">
                 <a class="navbar-brand font-weight-bold" href="/welcome">D-Shop</a>
-                    <form class="form-inline my-2 mx-auto search-bar">
-                        <input id="search" class="form-control pl-5" size="80" type="search" placeholder="Search Produk" aria-label="Search">
-                        <label for="search" class="label-search fa-solid fa-magnifying-glass blue-text ml-3"></label>
-                    </form>
-                <div class="mx-lg-5">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <form class="form-inline my-2 mx-auto search-bar">
+                    <input id="search" class="form-control pl-5" size="50" type="search" placeholder="Search Produk" aria-label="Search">
+                    <label for="search" class="label-search fa-solid fa-magnifying-glass blue-text ml-3"></label>
+                </form>
+                <div id="icon-cart" class="mx-lg-5">
                     <a href="#"><i class="fa-solid fa-cart-shopping text-white icon-size"></i></a>
                 </div>
-                <a class="btn btn-login px-sm-4 py-sm-1" href="#">LOGIN</a>
+                <a id="login-button" class="btn btn-login px-sm-4 py-sm-1" href="#">LOGIN</a>
+                </div>
             </div>
         </nav>
     </section>
