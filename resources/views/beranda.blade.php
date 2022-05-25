6c0527a784604d8d58a63d9f7240f9afbcce35e7
akuutauf <taufikhidayat09121@gmail.com>
2022-05-25 15:22:35 +0700

    Add Navbar none responsif

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,17 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Beranda | D-Shop</title>
-    {{-- Css Bootstrap --}}
+    {{-- Font awesome cdn --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- Css custom --}}
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    {{-- Css bootstrap --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
     {{-- Navabar --}}
     <section id="navbar">
-        <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-            <!-- Navbar content -->
-          </nav>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-navbar fixed-top shadow">
+            <div class="container d-flex justify-content-lg-around align-items-center">
+                <a class="navbar-brand font-weight-bold" href="/welcome">D-Shop</a>
+                    <form class="form-inline my-2 mx-auto search-bar">
+                        <input id="search" class="form-control pl-5" size="80" type="search" placeholder="Search Produk" aria-label="Search">
+                        <label for="search" class="label-search fa-solid fa-magnifying-glass blue-text ml-3"></label>
+                    </form>
+                <div class="mx-lg-5">
+                    <a href="#"><i class="fa-solid fa-cart-shopping text-white icon-size"></i></a>
+                </div>
+                <a class="btn btn-login px-sm-4 mx-4 py-sm-1" href="#">LOGIN</a>
+            </div>
+        </nav>
     </section>
 
+    {{-- Font awesome ions kit --}}
+    <script src="https://kit.fontawesome.com/dee340b044.js" crossorigin="anonymous"></script>
     {{-- Bootstrap JS --}}
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
