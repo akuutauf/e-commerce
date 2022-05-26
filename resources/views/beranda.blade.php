bd5a3701d11eb1c15a073c312d9695ff4896f88d
akuutauf <taufikhidayat09121@gmail.com>
2022-05-26 15:44:14 +0700

    Fix Array Produk Shoes

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -7,80 +7,32 @@
             <h3>Shoes</h3>
         </div>
         <div id="konten" class="container bg-white rounded-sm shadow-card">
-            <div class="row py-4 px-2">
-                <div class="col d-flex justify-content-center">
-                    <div class="card shadow-card" style="width: 15.5rem;">
-                        <img class="card-img-top" src="{{ asset('img/shoes_01.jpg') }}" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="h6 card-text">Sneakers Sports - Green Edition Adidas</p>
-                            <p class="card-text product-price">Rp. 300.000</p>
-                            <div class="row">
-                                <div class="col">
-                                    <p class="h6 card-text">FashionStore<br><span class="card-text total-sales">Terjual
-                                            1rb+</span></p>
-                                </div>
-                                <div class="col">
-                                    <p class="card-text text-right shop-location">Banyuwangi</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col d-flex justify-content-center">
-                    <div class="card shadow-card" style="width: 15.5rem;">
-                        <img class="card-img-top" src="{{ asset('img/shoes_04.jpg') }}" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="h6 card-text">Sneakers Sports - Green Docker Edition Adidas</p>
-                            <p class="card-text product-price">Rp. 370.000</p>
-                            <div class="row">
-                                <div class="col">
-                                    <p class="h6 card-text">FashionStore<br><span class="card-text total-sales">Terjual
-                                            1rb+</span></p>
-                                </div>
-                                <div class="col">
-                                    <p class="card-text text-right shop-location">Banyuwangi</p>
+            @if (count($items) > 0)
+                <div class="row py-4 px-2">
+                    @foreach ($items as $item)
+                        <div class="col d-flex justify-content-center">
+                            <div class="card shadow-card" style="width: 15.5rem;">
+                                <img class="card-img-top" src="{{ $item[5] }}" alt="Card image cap">
+                                <div class="card-body">
+                                    <p class="h6 card-text">{{ $item[0] }}</p>
+                                    <p class="card-text product-price">Rp. {{ $item[1] }}</p>
+                                    <div class="row">
+                                        <div class="col-7">
+                                            <p class="card-text name-shop">{{ $item[2] }}<br><span
+                                                    class="card-text total-sales">Terjual
+                                                    {{ $item[3] }}</span></p>
+                                        </div>
+                                        <div class="col-5">
+                                            <p class="card-text text-right shop-location mt-4">
+                                                {{ $item[4] }}</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
-                <div class="col d-flex justify-content-center">
-                    <div class="card shadow-card" style="width: 15.5rem;">
-                        <img class="card-img-top" src="{{ asset('img/shoes_03.jpg') }}" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="h6 card-text">Sneakers Sports - Featuring Collab Adidas Nike</p>
-                            <p class="card-text product-price">Rp. 350.000</p>
-                            <div class="row">
-                                <div class="col">
-                                    <p class="h6 card-text">FashionStore<br><span class="card-text total-sales">Terjual
-                                            1rb+</span></p>
-                                </div>
-                                <div class="col">
-                                    <p class="card-text text-right shop-location">Banyuwangi</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col d-flex justify-content-center">
-                    <div class="card shadow-card" style="width: 15.5rem;">
-                        <img class="card-img-top" src="{{ asset('img/shoes_02.jpg') }}" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="h6 card-text">Sneakers Sports - Black Edition Adidas</p>
-                            <p class="card-text product-price">Rp. 355.000</p>
-                            <div class="row">
-                                <div class="col">
-                                    <p class="h6 card-text">FashionStore<br><span class="card-text total-sales">Terjual
-                                            1rb+</span></p>
-                                </div>
-                                <div class="col">
-                                    <p class="card-text text-right shop-location">Banyuwangi</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endif
         </div>
         <div id="konten" class="container my-5">
             <hr>
