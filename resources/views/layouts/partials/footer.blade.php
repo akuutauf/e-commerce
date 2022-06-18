5497f61d450b9a95e2285f5bdde4950a0c4f971a
akuutauf <taufikhidayat09121@gmail.com>
2022-06-18 19:21:05 +0700

    Mengubah konten footer fix

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,7 +3,6 @@
       <footer class="footer-color" id="tempaltemo_footer">
           <div class="container">
               <div class="row">
-
                   <div class="col-md-4 pt-5">
                       <h2 class="h2 text-white border-bottom pb-3 border-light logo title-footer">Obugame</h2>
                       <ul class="list-unstyled text-light footer-link-list pt-3">
@@ -21,7 +20,6 @@
                           </li>
                       </ul>
                   </div>
-
                   <div class="col-md-4 pt-5">
                       <h2 class="h2 text-light border-bottom pb-3 border-light title-footer">Produk</h2>
                       <ul class="list-unstyled text-light footer-link-list pt-3">
@@ -30,7 +28,6 @@
                           <li><a class="text-decoration-none text-white" href="#">Dragon Rengginang</a></li><br>
                       </ul>
                   </div>
-
                   <div class="col-md-4 pt-5">
                       <h2 class="h2 text-light border-bottom pb-3 border-light title-footer">E-Commerce</h2>
                       <ul class="list-unstyled text-light footer-link-list pt-3">
@@ -43,43 +40,41 @@
                           <li><a class="text-decoration-none text-white" href="/join-mitra">Bermitra Bersama Kami</a>
                       </ul>
                   </div>
-
               </div>
-
               <div class="row text-light mb-4">
                   <div class="col-12 mb-3">
                       <div class="w-100 my-3 border-top border-light"></div>
                   </div>
-                  <div class="col-auto me-auto">
+                  <div class="col-lg-3">
                       <ul class="list-inline text-left footer-icons">
-                          <li class="list-inline-item border border-light rounded-circle text-center  py-1 px-1">
+                          <li class="list-inline-item text-center py-1 px-1">
                               <a class="text-light text-decoration-none" target="_blank"
-                                  href="https://api.whatsapp.com/"><i class="fab fa-whatsapp fa-lg fa-fw"></i></a>
+                                  href="https://api.whatsapp.com/"><i
+                                      class="fa-brands fa-whatsapp menu-icon-footer"></i></a>
                           </li>
-                          <li class="list-inline-item border border-light rounded-circle text-center  py-1 px-1">
+                          <li class="list-inline-item text-center  py-1 px-1">
                               <a class="text-light text-decoration-none" target="_blank"
                                   href="https://www.instagram.com/obugame/"><i
-                                      class="fab fa-instagram fa-lg fa-fw"></i></a>
+                                      class="fa-brands fa-instagram menu-icon-footer"></i></a>
                           </li>
-                          <li class="list-inline-item border border-light rounded-circle text-center py-1 px-1">
+                          <li class="list-inline-item text-center py-1 px-1">
                               <a class="text-light text-decoration-none" target="_blank"
                                   href="https://www.facebook.com/sahabatobugamenia/"><i
-                                      class="fab fa-facebook-f fa-lg fa-fw"></i></a>
+                                      class="fa-brands fa-facebook menu-icon-footer"></i></a>
                           </li>
-                          <li class="list-inline-item border border-light rounded-circle text-center py-1 px-1 mb-1">
+                          <li class="list-inline-item text-center py-1 px-1 mb-1">
                               <a class="text-light text-decoration-none" target="_blank"
                                   href="https://shopee.co.id/elladeffi?page=0">
-                                  <img height="24.5" width="24.5" src="{{ asset('icons/shopee.png') }}"
-                                      alt="">
+                                  <i class="fa-brands fa-shopify menu-icon-footer"></i>
                               </a>
                           </li>
                       </ul>
                   </div>
-                  <div class="col-9">
+                  <div class="col-lg-9">
                       <p class="text-right text-light">
                           Copyrights &copy; 2022 Obugame
                           | Designed by <a rel="sponsored" href="" target="_blank"
-                              class="text-white"><b>OBUGAMI</b></a>
+                              class="text-white"><b>OBUGAME</b></a>
                       </p>
                   </div>
               </div>
